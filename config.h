9c4525f5d61b349acce2ab3620e8aedb5b1a6be7
liu.bian.shi <liu.bian.shi@gmail.com>
2022-02-02 00:16:52 +0800

    personal script

--- config.h
+++ config.h
@@ -1,16 +1,20 @@
-#define CMDLENGTH 60
-#define DELIMITER "  "
+#define CMDLENGTH 80
+#define DELIMITER " "
 #define CLICKABLE_BLOCKS
+// #define LEADING_DELIMITER ""
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	//BLOCK("sb-mail",    1800, 17),
+	//BLOCK("sb-music",   0,    18),
+	//BLOCK("sb-disk",    1800, 19),
+	//BLOCK("sb-memory",  10,   20),
+	//BLOCK("sb-loadavg", 5,    21),
+	//BLOCK("sb-mic",     0,    26),
+	//BLOCK("sb-record",  0,    27),
+	//BLOCK("sb-volume",  0,    22),
+	//BLOCK("sb-battery", 5,    23),
+	//BLOCK("sb-date",    1,    24)
+    BLOCK("weather",    18000, 1),
+    BLOCK("memory",     10, 0),
+    BLOCK("clock",      60, 0)
 };
