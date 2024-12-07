bc5176c1fff80227b8c7395e4d88037e2b73224d
liu.bian.shi <liu.bian.shi@gmail.com>
2024-12-07 11:30:35 +0800

    ---

--- config.h
+++ config.h
@@ -6,15 +6,16 @@
 const Block blocks[] = {
 	//BLOCK("sb-mail",    1800, 17),
 	//BLOCK("sb-music",   0,    18),
-	//BLOCK("sb-disk",    1800, 19),
 	//BLOCK("sb-memory",  10,   20),
 	//BLOCK("sb-loadavg", 5,    21),
 	//BLOCK("sb-mic",     0,    26),
 	//BLOCK("sb-record",  0,    27),
-	//BLOCK("sb-volume",  0,    22),
-	//BLOCK("sb-battery", 5,    23),
 	//BLOCK("sb-date",    1,    24)
-    BLOCK("weather",    18000, 1),
-    BLOCK("memory",     10, 0),
-    BLOCK("clock",      60, 0)
+  BLOCK("cpu",          10,    0),
+  BLOCK("memory",       10,    0),
+	BLOCK("db-disk",      1800,  0),
+  BLOCK("battery_info", 5,     0),
+	BLOCK("volume",       0,     0),
+  BLOCK("weather",      18000, 0),
+  BLOCK("clock",        60,    0)
 };
